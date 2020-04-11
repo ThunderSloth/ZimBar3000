34f18660f5dd22487d96de17b6af8456ca3abd74
Eli Bell <elibell@elibell.net>
2020-04-11 17:39:02 -0400

    removed print

--- ZimBar3000/Map_Door_Text/Map_Door_Text.lua
+++ ZimBar3000/Map_Door_Text/Map_Door_Text.lua
@@ -24,7 +24,6 @@ end
 -- load variables
 function mdt_get_variables()
 	quowmap_database =  GetPluginInfo(GetPluginID (), 20):gsub("\\([A-Za-z_]+)\\$", "\\shared\\").."_quowmap_database.db"
-	print(quowmap_database)
 	FIXED_TITLE_HEIGHT = 16
     assert(loadstring(GetVariable("window_width" ) or ""))()
     assert(loadstring(GetVariable("window_height") or ""))()

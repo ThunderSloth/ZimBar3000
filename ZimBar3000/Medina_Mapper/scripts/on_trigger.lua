63c563e2aaa74d3e30161c6c149e8babae1c5c66
Eli Bell <elibell@elibell.net>
2020-04-10 23:23:05 -0400

    begin work on mobs

--- ZimBar3000/Medina_Mapper/scripts/on_trigger.lua
+++ ZimBar3000/Medina_Mapper/scripts/on_trigger.lua
@@ -31,7 +31,7 @@ function on_trigger_medina_room(name, line, wildcards, styles)
     if wildcards.title ~= '' then
 		medina_reset_thyngs(room)
         if wildcards.thyngs ~= '' then
-            on_trigger_medina_mob_track("here", line, {thyngs = wildcards.thyngs}, styles, room)
+            on_trigger_medina_mob_track("here", line, {thyngs = wildcards.thyngs}, styles, med.sequence[1])
         else
             medina_move_room(room, exits)
         end
@@ -41,7 +41,7 @@ function on_trigger_medina_room(name, line, wildcards, styles)
             on_trigger_medina_dark_room(name, line, wildcards, styles)
         else
             if wildcards.thyngs ~= '' then
-                on_trigger_medina_mob_track("there", line, {thyngs = wildcards.thyngs}, styles, room)
+                on_trigger_medina_mob_track("there", line, {thyngs = wildcards.thyngs}, styles, med.look_room)
             else
                 medina_look_room(room, exits)
             end
@@ -54,7 +54,7 @@ function on_trigger_medina_dark_room(name, line, wildcards, styles)
     local current_room = med.sequence[1]
     if wildcards.title ~= '' then
         if wildcards.thyngs ~= '' then
-            on_trigger_medina_mob_track("here", line, {thyngs = wildcards.thyngs}, styles, room)
+            on_trigger_medina_mob_track("here", line, {thyngs = wildcards.thyngs}, styles, med.sequence[1])
         else
             local exits  = medina_exit_string_to_list(wildcards.exits)
             local room = medina_get_room(current_room, exits)
@@ -62,7 +62,7 @@ function on_trigger_medina_dark_room(name, line, wildcards, styles)
         end
     elseif wildcards.look ~= '' then
         if wildcards.thyngs ~= '' then
-            on_trigger_medina_mob_track("there", line, {thyngs = wildcards.thyngs}, styles, room)
+            on_trigger_medina_mob_track("there", line, {thyngs = wildcards.thyngs}, styles, med.look_room)
         else
             local exits = medina_exit_string_to_list(wildcards.exits)
             local room = medina_get_room(current_room, exits)

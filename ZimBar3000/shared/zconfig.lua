d733a04d1e11b751d00b24abed40997450249612
Eli Bell <elibell@elibell.net>
2020-04-19 23:21:16 -0400

    fixed bug in config

--- ZimBar3000/shared/zconfig.lua
+++ ZimBar3000/shared/zconfig.lua
@@ -94,8 +94,8 @@ function run_mud_config()
     function on_trigger_option(name, line, wildcards, styles)
         local k1, k2 = name:match("^(.*)_(%w+)$")
         k1 = k1:gsub("_", " ")
-        if not wildcards.option:match(options[k1][k2]) then
-            zconfig = zconfig..(k1.." "..k2.." = "..options[k1][k2])..";"
+        if not wildcards.option:match(mud_options[k1][k2]) then
+            zconfig = zconfig..(k1.." "..k2.." = "..mud_options[k1][k2])..";"
         end
     end
     
@@ -110,24 +110,24 @@ function run_mud_config()
         if #zconfig == 0 then
             print("\nYou are already configured!")
         else
-            check(AddTrigger("zconfig_added", '^[>]?\\w+ alias "zconfig"', "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_zconfig_added"))
+            AddTrigger("zconfig_added", '^[>]?\\w+ alias "zconfig"', "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_zconfig_added")
             Send("alias zconfig "..zconfig.."unalias zconfig")
         end
     end
     
     function on_trigger_zconfig_added(name, line, wildcards, styles)
-        check(AddTrigger("zconfig_removed", '^[>]?Successfully unaliased "zconfig":.*$', "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_zconfig_removed"))
+        AddTrigger("zconfig_removed", '^[>]?Successfully unaliased "zconfig":.*$', "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_zconfig_removed")
         Send("zconfig")
     end    
     
     function on_trigger_zconfig_removed(name, line, wildcards, styles)
 		if was_player_colour_altered then
-			print("Your 'playername' colour option must be set.\nIt has been changed to: cyan. You may change it to any colour you'd like.\nIt is also reccomended that you set 'groupmate' and 'playerkiller' too, although not required.")
+			Note("Your 'playername' colour option must be set.\nIt has been changed to: cyan. You may change it to any colour you'd like.\nIt is also reccomended that you set 'groupmate' and 'playerkiller' too, although not required.")
 		end
-		print("\nConfiguration complete!") 
+		Note("Configuration complete!") 
     end
     
-    options = {
+    mud_options = {
         ["options terminal"] = {
             type = "network",
         },
@@ -162,22 +162,22 @@ function run_mud_config()
 	was_player_colour_altered = false
 	
     zoption, zconfig = "", ""
-     for k1, t in pairs(options) do
+     for k1, t in pairs(mud_options) do
         for k2, v in pairs(t) do
             local m = "^[>]?\\s+"..k2.."\\s+= (?<option>\\w+) .*$"
             local name = (k1.."_"..k2):gsub(" ", "_")
-            check(AddTrigger(name, m, "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_option"))
+            AddTrigger(name, m, "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_option")
         end
         zoption = zoption..k1..";"
     end
     
-    check(AddTrigger("playername", "^[>]?Colour playername\\s+= [[](?<p_colour>\\w+)[]].*", "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_playername"))
+    AddTrigger("playername", "^[>]?Colour playername\\s+= [[](?<p_colour>\\w+)[]].*", "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_playername")
 
-    check(AddTrigger("zoption_removed", '^[>]?Successfully unaliased "zoption":.*$', "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_zoption_removed"))
+    AddTrigger("zoption_removed", '^[>]?Successfully unaliased "zoption":.*$', "", trigger_flag.Enabled + trigger_flag.Temporary + trigger_flag.RegularExpression + trigger_flag.OneShot + trigger_flag.KeepEvaluating + trigger_flag.Replace, -1, 0, "", "on_trigger_zoption_removed")
         
     Send("alias zoption "..zoption.."options colour playername;".."unalias zoption");Send("zoption")
     
 end
-
+Note("Running configuration:")
 run_mush_config()
 run_mud_config()
